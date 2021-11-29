e0ee5a81882f6c3f2b51b61842315580aa4d50ec
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-11-29 08:49:39 +0000

    Add test for layer time

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -1,3 +1,8 @@
+def test_layer_time_is_extracted(feature_layer):
+    feature_layer.extract_layer_time()
+    assert feature_layer.layer_time == '13:00'
+
+
 def test_shapefile_is_downloaded(feature_layer):
     feature_layer.download_shapefile()
     assert feature_layer.layer_file.exists()
